842266e2ffe22f67a41d72b2e1e322fbe4d513c1
leeye <815933539@qq.com>
2017-03-29 11:11:28 +0800

    提交tv

--- app/src/main/java/com/leeye/newsapp/MainActivity.java
+++ app/src/main/java/com/leeye/newsapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.leeye.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.tv);
+
+        mTextView.setText("你好");
+
     }
 }
